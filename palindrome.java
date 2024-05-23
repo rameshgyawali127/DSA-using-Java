ff78a3da4af5fb8f0ea0f2fd1d0d9c6d02aa1511
Ramesh Gyawali <gamingrowmexh@gmail.com>
2024-05-23 17:15:51 +0545

    done palindrome which contain symbols and number as a string

---
+++ palindrome.java
@@ -0,0 +1,24 @@
+// 125. Valid Palindrome
+class palindrome{
+    public static void main(String[] args) {
+        String s ="A man, a plan, a canal: Panama";
+        // String s = "I am Ramesh Gyawali";
+        System.out.println(isPalindrome(s));
+    }
+    public static boolean isPalindrome(String s) {
+        s = s.replaceAll("[^a-zA-Z0-9]", "").toLowerCase();// we have to remove all symbols, and change our character into lower case!
+        int start = 0;
+        int end = s.length() -1 ;
+
+        while(start <= end ){
+            if(s.charAt(start) == s.charAt(end)){
+                start ++;
+                end --;
+            }
+            else{
+                return false;
+            }
+        }
+        return true;
+    }
+}
