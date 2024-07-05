56bcb3e236e88cfd85ead99a02fa79ee83277b82
Ramesh Gyawali <gamingrowmexh@gmail.com>
2024-07-05 11:43:10 +0530

    update end logic with another trick

--- FindTargetFromInfiniteArray.java
+++ FindTargetFromInfiniteArray.java
@@ -3,24 +3,22 @@ public class FindTargetFromInfiniteArray {
         int start = 0,mid =0;
         int end = 1;
         while(end < arr.length && arr[end] < target){
-            start = end + 1;
-            end = (end *2 < arr.length-1)? end*2 : arr.length-1;
-
+            start = end;
+            end = (end + (end -start + 1)*2 < arr.length-1)?(end + (end -start + 1)*2):arr.length-1;//end + size of box? make sense!
+            // end = (end*2 > arr.length-1)?end*2:arr.length-1;
         }
-        while(end >= start){
+        while(start <= end){
             mid = start + (end-start)/2;
-            if(target < arr[end]){
                 if(arr[mid] > target){
                     end = mid-1;
-                }
-                else if(arr[mid] < target){
-                    start = mid +1;
                 }
                 else if (arr[mid] == target){
                     return mid;
                 }
+                else if(arr[mid] < target){
+                    start = mid +1;
+                }
             }
-        }
         return -1;
     }
     public static void main(String[] args) {
